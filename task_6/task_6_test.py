1244848586361eff5b59ed7b49d63011790e3949
Maryia Samusionak <Maryia_Samusionak@epam.com>
2018-06-02 22:08:21 +0300

    Change task6

--- task_6/task_6_test.py
+++ task_6/task_6_test.py
@@ -1,13 +1,8 @@
 import unittest
-from task_6_program import *
+from task_6_program import poly, count_characters, fizzbuzz
 
 
 class Test(unittest.TestCase):
-    def test_poly(self):
-        self.assertFalse(poly(8855332))
-        self.assertTrue(poly('cakekac'))
-        self.assertTrue(poly())
-
     def test_count_characters(self):
         example = {'H': 1, 'e': 1, 'l': 3, 'o': 2,
                    ',': 1, ' ': 1, 'w': 1, 'r': 1, 'd': 1}
@@ -20,6 +15,11 @@ class Test(unittest.TestCase):
                                         'Fizz', '7', '8', 'Fizz', 'Buzz',
                                         '11', 'Fizz', '13', '14', 'FizzBuzz'])
         self.assertEqual(fizzbuzz(0), [])
+
+    def test_poly(self):
+        self.assertFalse(poly(8855332))
+        self.assertTrue(poly('cakekac'))
+        self.assertTrue(poly())
 
 
 if __name__ == '__main__':
